787c5f9b056b0fae472f7b2b55fbc0ce85ac58db
Shaikh Aquib <shaikhaquib119@gmail.com>
2018-10-18 17:37:11 +0530

    Aquib

--- app/build/generated/source/r/debug/android/support/mediacompat/R.java
+++ app/build/generated/source/r/debug/android/support/mediacompat/R.java
@@ -53,18 +53,18 @@ public final class R {
         public static final int notification_top_pad_large_text = 0x7f06009d;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f07009c;
-        public static final int notification_bg = 0x7f07009d;
-        public static final int notification_bg_low = 0x7f07009e;
-        public static final int notification_bg_low_normal = 0x7f07009f;
-        public static final int notification_bg_low_pressed = 0x7f0700a0;
-        public static final int notification_bg_normal = 0x7f0700a1;
-        public static final int notification_bg_normal_pressed = 0x7f0700a2;
-        public static final int notification_icon_background = 0x7f0700a3;
-        public static final int notification_template_icon_bg = 0x7f0700a4;
-        public static final int notification_template_icon_low_bg = 0x7f0700a5;
-        public static final int notification_tile_bg = 0x7f0700a6;
-        public static final int notify_panel_notification_icon_bg = 0x7f0700a7;
+        public static final int notification_action_background = 0x7f07009f;
+        public static final int notification_bg = 0x7f0700a0;
+        public static final int notification_bg_low = 0x7f0700a1;
+        public static final int notification_bg_low_normal = 0x7f0700a2;
+        public static final int notification_bg_low_pressed = 0x7f0700a3;
+        public static final int notification_bg_normal = 0x7f0700a4;
+        public static final int notification_bg_normal_pressed = 0x7f0700a5;
+        public static final int notification_icon_background = 0x7f0700a6;
+        public static final int notification_template_icon_bg = 0x7f0700a7;
+        public static final int notification_template_icon_low_bg = 0x7f0700a8;
+        public static final int notification_tile_bg = 0x7f0700a9;
+        public static final int notify_panel_notification_icon_bg = 0x7f0700aa;
     }
     public static final class id {
         public static final int action0 = 0x7f080006;
@@ -77,27 +77,27 @@ public final class R {
         public static final int blocking = 0x7f080025;
         public static final int cancel_action = 0x7f08002c;
         public static final int chronometer = 0x7f080034;
-        public static final int end_padder = 0x7f080051;
-        public static final int forever = 0x7f08005b;
-        public static final int icon = 0x7f080062;
-        public static final int icon_group = 0x7f080063;
-        public static final int info = 0x7f080068;
-        public static final int italic = 0x7f08006a;
-        public static final int line1 = 0x7f08006f;
-        public static final int line3 = 0x7f080070;
-        public static final int media_actions = 0x7f080078;
-        public static final int normal = 0x7f08008b;
-        public static final int notification_background = 0x7f08008c;
-        public static final int notification_main_column = 0x7f08008d;
-        public static final int notification_main_column_container = 0x7f08008e;
-        public static final int right_icon = 0x7f0800a0;
-        public static final int right_side = 0x7f0800a1;
-        public static final int status_bar_latest_event_content = 0x7f0800ca;
-        public static final int tag_transition_group = 0x7f0800cf;
-        public static final int text = 0x7f0800d1;
-        public static final int text2 = 0x7f0800d2;
-        public static final int time = 0x7f0800d9;
-        public static final int title = 0x7f0800da;
+        public static final int end_padder = 0x7f080050;
+        public static final int forever = 0x7f08005a;
+        public static final int icon = 0x7f080061;
+        public static final int icon_group = 0x7f080062;
+        public static final int info = 0x7f080067;
+        public static final int italic = 0x7f080069;
+        public static final int line1 = 0x7f08006e;
+        public static final int line3 = 0x7f08006f;
+        public static final int media_actions = 0x7f080077;
+        public static final int normal = 0x7f08008a;
+        public static final int notification_background = 0x7f08008b;
+        public static final int notification_main_column = 0x7f08008c;
+        public static final int notification_main_column_container = 0x7f08008d;
+        public static final int right_icon = 0x7f08009f;
+        public static final int right_side = 0x7f0800a0;
+        public static final int status_bar_latest_event_content = 0x7f0800c9;
+        public static final int tag_transition_group = 0x7f0800ce;
+        public static final int text = 0x7f0800d0;
+        public static final int text2 = 0x7f0800d1;
+        public static final int time = 0x7f0800d8;
+        public static final int title = 0x7f0800d9;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090004;
@@ -121,7 +121,7 @@ public final class R {
         public static final int notification_template_part_time = 0x7f0a0044;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0d0059;
+        public static final int status_bar_notification_info_overflow = 0x7f0d005a;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f0e00f4;
--- app/src/main/java/com/krayrr/Activity/MapsActivity.java
+++ app/src/main/java/com/krayrr/Activity/MapsActivity.java
@@ -1,177 +1,269 @@
 package com.krayrr.Activity;
 
+
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
+import android.location.Criteria;
 import android.location.Location;
-import android.os.Build;
-import android.os.Looper;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.krayrr.Model.RideRoutes;
 import com.krayrr.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 5445;
-
-    private GoogleMap googleMap;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private Marker currentLocationMarker;
-    private Location currentLocation;
-    private boolean firstTimeFlag = true;
-
-   /* private final View.OnClickListener clickListener = view -> {
-        if (view.getId() == R.id.currentLocationImageButton && googleMap != null && currentLocation != null)
-            animateCamera(currentLocation);
-    };
-*/
-    private final LocationCallback mLocationCallback = new LocationCallback() {
-
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            super.onLocationResult(locationResult);
-            if (locationResult.getLastLocation() == null)
-                return;
-            currentLocation = locationResult.getLastLocation();
-            if (firstTimeFlag && googleMap != null) {
-                animateCamera(currentLocation);
-                firstTimeFlag = false;
-            }
-            showMarker(currentLocation);
-        }
-    };
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapsActivity";
+    public GoogleMap mMap;
+    private ArrayList<LatLng> points;
+    Polyline line;
+    Marker now;
+    double lat1;
+    double lon1;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    ArrayList<RideRoutes> ride_route_list = new ArrayList();
+    RideRoutes rideRoutes =new RideRoutes();
+    List<Float> list = new ArrayList<>();
+    SupportMapFragment mapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        points = new ArrayList<LatLng>();
         setContentView(R.layout.activity_maps);
-        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
-        supportMapFragment.getMapAsync(this);
-       // findViewById(R.id.currentLocationImageButton).setOnClickListener(clickListener);
+
+
+
+         mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+
     }
 
+
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
+
+        mMap = googleMap;
+        if (!mMap.isMyLocationEnabled())
+            mMap.setMyLocationEnabled(true);
+
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        Location myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+      //  mMap.getUiSettings().setZoomControlsEnabled(false);
+
+
+        if (myLocation == null) {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            String provider = lm.getBestProvider(criteria, true);
+            myLocation = lm.getLastKnownLocation(provider);
+        }
+
+        if (myLocation != null) {
+            LatLng userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+            lat1 = myLocation.getLatitude();
+            lon1 = myLocation.getLongitude();
+
+
+            mMap.addMarker(new MarkerOptions()
+                    .position(userLocation)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                    .title("Welcome ")
+                    .snippet("Latitude:" + lat1 + ",Longitude:" + lon1)
+            );
+
+            Log.v(TAG, "Lat1=" + lat1);
+            Log.v(TAG, "Long1=" + lon1);
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 20), 1500, null);
+
+
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location myLocation) {
+
+                    // Getting latitude of the current location
+                    final double latitude = myLocation.getLatitude();
+
+                    // Getting longitude of the current location
+                    final double longitude = myLocation.getLongitude();
+
+                    // Creating a LatLng object for the current location
+                    LatLng latLng = new LatLng(latitude, longitude);
+
+
+                    //Adding new marker
+                 /*   now = mMap.addMarker(new MarkerOptions()
+                            .icon(BitmapDescriptorFactory
+                                    .defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                            .position(latLng).title("New")
+                            .snippet("Latitude:"+lat1+",Longitude:"+lon1)
+                    );*/
+
+                    // Showing the current location in Google Map
+                    //   mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                    // Zoom in the Google Map
+                    //   mMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+
+                    //Draw polyline
+                    drawPolygon(latitude, longitude);
+
+                    //   Toast.makeText(MapsActivity.this, String.valueOf(latitude)+String.valueOf(longitude), Toast.LENGTH_SHORT).show();
+                    //
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                    // TODO Auto-generated method stub
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                    // TODO Auto-generated method stub
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status,
+                                            Bundle extras) {
+                    // TODO Auto-generated method stub
+                }
+            });
+
+        }
+
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+      //  mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 
-    private void startCurrentLocationUpdates() {
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(3000);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(MapsActivity.this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+    private void drawPolygon(double latitude, double longitude) {
+
+        List<LatLng> polygon = new ArrayList<>();
+        //old lat and long
+        polygon.add(new LatLng(lat1, lon1));
+        //new lat and long
+
+
+        polygon.add(new LatLng(latitude, longitude));
+
+
+        mMap.addPolygon(new PolygonOptions()
+                .addAll(polygon)
+                .strokeColor(Color.parseColor("#03A9F4"))
+                .strokeWidth(15)
+                .fillColor(Color.parseColor("#B3E5FC"))
+        );
+
+        lat1 = latitude;
+        lon1 = longitude;
+
+
+        System.out.println("Distance :-" + getTripDistance(polygon));
+
+
+        list.add( getTripDistance(polygon));
+
+        System.out.println("Total Distance :-" + sum(list));
+
+
+
+    }
+    float getTripDistance(List<LatLng> vertices) {
+        float totalDistance = 0;
+
+        for (int i = 0; i < vertices.size()-1; i++) {
+            Location tLoc1 = new Location("");
+            Location tLoc2 = new Location("");
+
+            tLoc1.setLatitude(vertices.get(i).latitude);
+            tLoc1.setLongitude(vertices.get(i).longitude);
+
+            tLoc2.setLatitude(vertices.get(i + 1).latitude);
+            tLoc2.setLongitude(vertices.get(i + 1).longitude);
+
+            totalDistance += tLoc1.distanceTo(tLoc2);
+        }
+
+        return totalDistance;
+    }
+
+    public static int sum(List<Float> list) {
+        int sum = 0;
+        for (Float i: list) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    private void enableMyLocationIfPermitted() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocationIfPermitted();
+                } else {
+                    enableMyLocationIfPermitted();
+                }
                 return;
             }
+
         }
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, mLocationCallback, Looper.myLooper());
-    }
-
-    private boolean isGooglePlayServicesAvailable() {
-        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
-        int status = googleApiAvailability.isGooglePlayServicesAvailable(this);
-        if (ConnectionResult.SUCCESS == status)
-            return true;
-        else {
-            if (googleApiAvailability.isUserResolvableError(status))
-                Toast.makeText(this, "Please Install google play services to use this application", Toast.LENGTH_LONG).show();
-        }
-        return false;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
-            if (grantResults[0] == PackageManager.PERMISSION_DENIED)
-                Toast.makeText(this, "Permission denied by uses", Toast.LENGTH_SHORT).show();
-            else if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                startCurrentLocationUpdates();
-        }
-    }
-
-    private void animateCamera(@NonNull Location location) {
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(getCameraPositionWithBearing(latLng)));
-    }
-
-    @NonNull
-    private CameraPosition getCameraPositionWithBearing(LatLng latLng) {
-        return new CameraPosition.Builder().target(latLng).zoom(16).build();
-    }
-
-    private void showMarker(@NonNull Location currentLocation) {
-        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        if (currentLocationMarker != null)
-            currentLocationMarker.remove();
-       /* Context context = getApplicationContext();
-        Drawable drawable = context.getResources().getDrawable(R.drawable.ic_green_car_marker);
-// convert drawable to bitmap
-        Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
-// convert bitmap to drawable
-        Drawable d = new BitmapDrawable(bitmap);
-        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.ic_green_car_marker);*/
-
-        Drawable vectorDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_green_car_marker, null);
-        Bitmap bit = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
-                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bit);
-        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        vectorDrawable.draw(canvas);
-
-        currentLocationMarker = googleMap.addMarker(new MarkerOptions().icon( BitmapDescriptorFactory.fromBitmap(bit)).position(latLng));
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (fusedLocationProviderClient != null)
-            fusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (isGooglePlayServicesAvailable()) {
-            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-            startCurrentLocationUpdates();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        fusedLocationProviderClient = null;
-        googleMap = null;
     }
 }
